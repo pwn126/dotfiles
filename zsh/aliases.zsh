cdae39dcb6465b3d74079c7fcd0abe438be62ce3
w31mann <disclosed>
2024-04-16 10:03:15 +0200

    feat(zsh): some Zsh changes
    A few changes in environment and aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,12 +2,9 @@
 # shellcheck shell=bash
 
 alias fd="fd --unrestricted"
-alias feh="feh --scale-down --auto-zoom"
 alias ip="ip -color=auto"
 alias rg="rg --smart-case --unrestricted"
-alias sgit="ALL_PROXY=socks5h://localhost:666 git"
-alias sudo="doas "
-alias doas="doas "
+alias sudo="sudo "
 alias v=nvim
 alias vim=nvim
 alias vimdiff="nvim -d"
