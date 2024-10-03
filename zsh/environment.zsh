44920eee9aeef4c5dda4f601e98265263f3ddf64
user <user@eax.ninja>
2024-10-03 14:40:27 +0200

    feat(zsh): update environment vars
    Add cargo binary path to PATH varriable and do not nvidia related stuff
    for now.

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -2,12 +2,12 @@
 # shellcheck shell=bash
 
 export SHELL="/usr/bin/zsh"
-export LIBVA_DRIVER_NAME="vdpau"
-export VDPAU_DRIVER="nvidia"
+# export LIBVA_DRIVER_NAME="vdpau"
+# export VDPAU_DRIVER="nvidia"
 export EDITOR=nvim
 export SUDO_EDITOR="nvim"
 export DIFFPROG="nvim -d"
-export PATH="${PATH}:${HOME}/.local/cargo"
+export PATH="${PATH}:${HOME}/.cargo/bin"
 export SYSTEMD_EDITOR=nvim
 
 export STARSHIP_CONFIG="${HOME}/.config/zsh/starship.toml"
