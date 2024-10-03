591eb1a44f4476c34b35aed029c2926779bf5a4b
user <user@eax.ninja>
2024-10-03 14:40:26 +0200

    feat(nvim): add rust debugging
    Key binding to start a debugging sesion is still missing but generally
    working.

---
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,66 @@
+-- rustaceanvim - https://github.com/mrcjkb/rustaceanvim
+
+return {
+    "mrcjkb/rustaceanvim",
+    enabled = true,
+    version = "^4",
+    ft = { "rust" },
+    config = function()
+        vim.g.rustaceanvim = {
+            -- LSP configuration
+            server = {
+                on_attach = require("plugins.lsp.handlers").on_attach,
+                -- https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
+                default_settings = {
+                    cargo = {
+                        features = "all",
+                    },
+                    completion = {
+                        autoimport = {
+                            enable = false,
+                        },
+                        fullFunctionSignatures = {
+                            enable = true,
+                        },
+                    },
+                    diagnostics = {
+                        experimental = {
+                            enable = true,
+                        },
+                    },
+                    hover = {
+                        actions = {
+                            references = {
+                                enable = true,
+                            },
+                        },
+                    },
+                    check = {
+                        command = "clippy",
+                    },
+                    lens = {
+                        references = {
+                            adt = {
+                                enable = true,
+                            },
+                            enumVariant = {
+                                enable = true,
+                            },
+                            method = {
+                                enable = true,
+                            },
+                            trait = {
+                                enable = true,
+                            },
+                        },
+                    },
+                    rustfmt = {
+                        rangeFormatting = {
+                            enable = true,
+                        },
+                    },
+                },
+            },
+        }
+    end,
+}
