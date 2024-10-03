2dd9ca44516cc2e6141921848b983557f8e3d231
user <user@eax.ninja>
2024-10-03 14:40:27 +0200

    feat(nvim): change keybindings for oil and outline

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
     "stevearc/oil.nvim",
     keys = {
         {
-            "<leader>o",
+            "<leader><leader>o",
             "<cmd>Oil<cr>",
             desc = "Toggle Oil file explorer",
         },
