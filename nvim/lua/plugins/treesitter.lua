f1f66080b18b439582c0d5554bc977078ef125cb
user <user@eax.ninja>
2024-10-03 14:40:27 +0200

    feat(nvim): add query lang to treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -43,6 +43,7 @@ return {
                 "objdump",
                 "passwd",
                 "python",
+                "query",
                 "regex",
                 "rst",
                 "rust",
