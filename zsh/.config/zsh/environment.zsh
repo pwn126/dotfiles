0616b20c832d3c0a241b1943236ec134ba6654d3
user <user@eax.ninja>
2024-10-03 14:40:27 +0200

    chore(zsh): cleanup env variables

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -7,9 +7,6 @@ export SHELL="/bin/zsh"
 export EDITOR=nvim
 export SUDO_EDITOR="nvim"
 export DIFFPROG="nvim -d"
-export PATH="${PATH}:${HOME}/.local/bin"
-export PATH="${PATH}:/opt/homebrew/opt"
-export PATH="${PATH}:/opt/homebrew/opt/libtool/libexec/gnubin"
 
 export STARSHIP_CONFIG="${HOME}/.config/zsh/starship.toml"
 
@@ -17,11 +14,16 @@ export MANPAGER="less -R --use-color -Dd+r -Du+b"
 export MANROFFOPT="-c"
 
 export HOMEBREW_PREFIX="/opt/homebrew";
-export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-export HOMEBREW_REPOSITORY="/opt/homebrew";
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+export HOMEBREW_REPOSITORY="${HOMEBREW_PREFIX}";
+export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar";
 export HOMEBREW_NO_ANALYTICS=1
+
+export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:";
+export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}";
+
+export PATH="${PATH}:${HOMEBREW_PREFIX}/opt"
+export PATH="${PATH}:${HOMEBREW_PREFIX}/bin"
+export PATH="${PATH}:${HOMEBREW_PREFIX}/sbin"
+export PATH="${PATH}:${HOMEBREW_PREFIX}/opt/libtool/libexec/gnubin"
 
 source "$HOME/.cargo/env"
