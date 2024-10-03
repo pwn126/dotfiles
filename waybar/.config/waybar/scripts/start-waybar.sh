34508a36ab3910e274b883e994e80f20e8ae6c8e
user <user@eax.ninja>
2024-10-03 19:53:57 +0200

    fix(waybar): don't restart waybar if hyprland is not running anymore

--- waybar/.config/waybar/scripts/start-waybar.sh
+++ waybar/.config/waybar/scripts/start-waybar.sh
@@ -3,5 +3,15 @@
 set -eE -o pipefail
 
 while true; do
-    waybar || echo "Waybar crashed! Restarting!" | systemd-cat --identifier="waybar" --priority=err
+    waybar || {
+        echo "Waybar crashed!" | systemd-cat --identifier="waybar" --priority=err
+
+        hyprland_pid=$(ps x |rg Hyprland | awk '$5 == "Hyprland" {print $1}')
+
+        if [[ -z "${hyprland_pid}" ]]; then
+            return 0
+        fi
+
+        sleep 1
+    }
 done
