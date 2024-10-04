be979b5834e895b622dcf25a0488b1cebca9ea8e
user <user@eax.ninja>
2024-10-04 13:16:35 +0200

    feat(zsh): two line prompt

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -2,15 +2,15 @@
 # shellcheck shell=bash
 
 export SHELL="/usr/bin/zsh"
-# export LIBVA_DRIVER_NAME="vdpau"
-# export VDPAU_DRIVER="nvidia"
 export EDITOR=nvim
+export BROWSER="/usr/bin/zen-browser"
 export SUDO_EDITOR="nvim"
 export DIFFPROG="nvim -d"
-export PATH="${PATH}:${HOME}/.cargo/bin"
 export SYSTEMD_EDITOR=nvim
-
-export STARSHIP_CONFIG="${HOME}/.config/zsh/starship.toml"
 
 export MANPAGER="less -R --use-color -Dd+r -Du+b"
 export MANROFFOPT="-c"
+
+export PATH="${PATH}:${HOME}/.cargo/bin"
+
+export STARSHIP_CONFIG="${HOME}/.config/zsh/starship.toml"
