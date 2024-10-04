708d5dd51de5a944aabf7e1cd2184057cfd367db
user <user@eax.ninja>
2024-10-04 13:15:11 +0200

    feat(nvim): enable border for LspInfo win

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,10 @@ return {
         local lspconfig = require("lspconfig")
         local handlers = require("plugins.lsp.handlers")
 
+        require("lspconfig.ui.windows").default_options = {
+            border = "single"
+        }
+
         handlers.setup()
 
         local servers = {
